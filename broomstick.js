deafeeadc490839d4784a11244a70f266a852a5a
Lawrence Grant <lawrencekgrant@gmail.com>
2012-09-23 20:26:43 -0700

    Add gzip support
    Here I'm just adding gzip to the options and if it is enabled, then we
    run the file through a gzip before sending it to the cache and the
    user. I also store the encoding in the cache object so multiple
    encoding types can be supported. Just incase deflate were to be added.

--- broomstick.js
+++ broomstick.js
@@ -6,20 +6,23 @@ var mime = require('mime');
 var fs = require('fs');
 var path = require('path');
 var colors = require('colors');
+var zlib = require('zlib');
 
 var Broomstick = function (options) {
   options = options || {};
   this.path = options.path || 'public';
   this.index = options.index || 'index.html';
   this.verbose = options.verbose || false;
+  this.gzip = options.gzip || false;
   this.cache = {};
+
   var b = this;
   b.log('initialized');
 
   return function (route) {
     var req = this.req;
     var res = this.res;
-    res.setHeader('X-Powered-By', 'broomstick');
+    res.setHeader('X-Powered-By', 'broomstick-gz');
 
     if (!route) route = b.index;
 
@@ -27,8 +30,12 @@ var Broomstick = function (options) {
 
     if (b.cache[route]) {
       b.log('req', 'cached version exists for ' + route + ' of type ' + b.cache[route].mime);
-      res.writeHead(200, { 'Content-Type': b.cache[route].mime })
-      res.end(b.cache[route].data);
+      res.writeHead(200, {
+          'Content-Type': b.cache[route].mime,
+          'Content-Encoding' : b.cache[route].encoding});
+      res.write(b.cache[route].data, b.cache[route].encoding);
+      res.end();
+
     } else {
       var filepath = path.join(b.path, route);
       b.log('req', 'no cached version for ' + route);
@@ -40,13 +47,22 @@ var Broomstick = function (options) {
           res.end();
         } else {
           var type = mime.lookup(path.extname(route));
-          res.writeHead(200, { 'Content-Type': type })
-          var fileStream = fs.createReadStream(filepath);
+          var encoding = (b.gzip ? 'gzip' : '');
+          res.writeHead(200, { 'Content-Type': type, 'Content-Encoding': encoding});
+
+          //if gzip is enabled, pass the file through a gzipper
+          var fileStream = (b.gzip ?
+            fs.createReadStream(filepath).pipe(zlib.createGzip()) :
+            fs.createReadStream(filepath));
+
+            //we can only one buffer per encoding, so, we'll change the name of the route to match
+          route = route + encoding;
           b.cache[route] = {
-            mime: type
+            mime: type,
+            encoding: encoding
           }
           fileStream.on('data', function (data) {
-            res.write(data);
+            res.write(data, encoding);
             if (!b.cache[route].data) {
               b.cache[route].data = data;
             } else {
